ad1ad40696b7468eaebca755a85676bf7c4deb94
Manaswini <2mpattanaik@gmail.com>
2026-01-26 10:13:43 +1100

    Added Port Info

--- test_home_page/test_login.py
+++ test_home_page/test_login.py
@@ -4,23 +4,23 @@ from playwright.sync_api import Page, expect
 
 
 def test_homepage(page:Page):
-    page.goto("http://127.0.0.1:9088/")
+    page.goto("http://127.0.0.1:5001/")
     time.sleep(2)
     expect(page.locator(".lead")).to_be_visible()
     time.sleep(2)
 
 def test_loginButton_present(page:Page):
-    page.goto("http://127.0.0.1:9088/")
+    page.goto("http://127.0.0.1:5001/")
     expect(page.get_by_role("link",name="Login")).to_be_visible()
     time.sleep(5)
 
 def test_resgisterButton_present(page:Page):
-    page.goto("http://127.0.0.1:9088/")
+    page.goto("http://127.0.0.1:5001/")
     expect(page.get_by_role("link",name="Register")).to_be_visible()
     time.sleep(5)
 
 def test_egistrationPositive(page:Page):
-    page.goto("http://127.0.0.1:9088/")
+    page.goto("http://127.0.0.1:5001/")
     page.get_by_role("link",name="Register").click()
     expect(page.get_by_role("heading",name="Register")).to_be_visible()
     page.locator('input[name="username"]').fill("test")
@@ -30,7 +30,7 @@ def test_egistrationPositive(page:Page):
     time.sleep(5)
 
 def test_login(page:Page):
-    page.goto("http://127.0.0.1:9088/")
+    page.goto("http://127.0.0.1:5001/")
     page.get_by_role("link",name="Login").click()
     page.locator('input[name="username"]').fill("test")
 
