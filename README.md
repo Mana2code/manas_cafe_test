d543176c2c7fc5673cd1431b0214554e710e0798
Manaswini <2mpattanaik@gmail.com>
2026-01-27 10:40:38 +1100

    beverage1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 
 pytest --junit-xml=../reports/junit.xml --html=../reports/report.html --self-contained-html
+
+
+ git add .
+ 
+ git commit -am "Added Version0 Beverage Page"
+ 
+git push
--- test_menu_page/test_beverages.py
+++ test_menu_page/test_beverages.py
@@ -8,3 +8,20 @@ def test_homepage(page:Page):
     page.goto("http://127.0.0.1:5001/")
 
     expect(page.locator(".lead")).to_be_visible()
+
+def test_login_Page(page: Page):
+    page.goto("http://127.0.0.1:5001/")
+    with open("data/credentials.json") as f:
+        test_data=json.load(f)
+        page.get_by_role("link",name="Login").click()
+        userName = test_data["user_crendentials"]["userName"]
+        userPassword = test_data["user_crendentials"]["userPassword"]
+        page.locator('input[name="username"]').fill(userName)
+        page.locator('input[name="password"]').fill(userPassword)
+        time.sleep(3)
+        page.get_by_role("button",name="Login").click()
+        #expect(page.get_by_text("Beverage Catalogue")).to_be_visible()
+        time.sleep(2)
+
+
+
